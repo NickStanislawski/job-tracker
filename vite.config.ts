d84f63062a6669d063c8c10ba24245a5976fa925
Nick <nickstan1110@gmail.com>
2026-07-07 20:59:41 -0400

    Add gh-pages config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  // Relative base so the built app works at https://<user>.github.io/<repo>/
-  // without needing to hardcode the repo name here.
-  base: "./",
+  base: '/job-tracker/',
 });
